5cc1b3ad0788d4acba96ea809a82e98eab829411
Cassandra O'Connell <oconnecl@gmail.com>
2022-02-15 18:19:40 -0500

    Adds basic app structure and removes unneeded boilerplate.

--- pages/add-book.tsx
+++ pages/add-book.tsx
@@ -2,18 +2,18 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
-const Home: NextPage = () => {
+const AddBook: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>mmhmm Bookshelf</title>
-        <meta name="description" content="Look at all these books!" />
+        <title>mmhmm Bookshelf - Add Book</title>
+        <meta name="description" content="Wow! I can add more books?" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>TODO - Bookshelf</main>
+      <main className={styles.main}>TODO - Add Book</main>
     </div>
   );
 };
 
-export default Home;
+export default AddBook;
